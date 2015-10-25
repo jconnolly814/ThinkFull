2f06b09f3ff61f6b9ee2d6bd514769f6ca885987
Jennifer Connolly <je.connolly@gmail.com>
2015-10-25 19:25:00 -0400

    education

--- education.py
+++ education.py
@@ -6,7 +6,7 @@ import sqlite3 as lite
 import matplotlib.pyplot as plt
 import csv
 import numpy as np
-import statsmodels.api as sm
+#import statsmodels.api as sm
 
 url = "http://web.archive.org/web/20110514112442/http://unstats.un.org/unsd/demographic/products/socind/education.htm"
 
@@ -22,45 +22,55 @@ soup_data = BeautifulSoup(r.content)
 
 
 soup_tag = soup_data('table')[6].tr.td
+#for row in soup_tag:
+    #print row
 soup_table = soup_tag('table')[1].tr.td.div
+#for row in soup_table:
+    #print row
 raw_table = soup_table('table')[0]
+#for row in raw_table:
+    #print row
 
 
 
 col_name = []
 
-for child in raw_table('tr'):
-    if child.get('class', ['Null'])[0] == 'lheader':
-        for td in child.find_all('td'):
+for j in raw_table('tr'):
+    if j.get('class', ['Null'])[0] == 'lheader':
+        for td in j.find_all('td'):
             if td.get_text() != '':
                 col_name.append(td.get_text())
         break
 
 
 country_table = pd.DataFrame(columns=col_name)
-print country_table
+#print country_table
 
-edu_row_num = 0
-for child in raw_table('tr'):
-    row_curr = []
-    if child.get('class', ['Null']) == 'tcont':
-        row_curr.append(child.find('td').get_text())
-        for td in child.find_all('td')[1:]:
+edu_counter = 0
+for j in raw_table('tr'):
+    row_data = []
+    if j.get('class', ['Null']) == 'tcont':
+        row_data.append(j.find('td').get_text())
+        for td in j.find_all('td')[1:]:
             if td.get('align'):
-                row_curr.append(td.get_text())
+                row_data.append(td.get_text())
     else:
-        row_curr.append(child.find('td').get_text())
-        for td in child.find_all('td')[1:]:
+        row_data.append(j.find('td').get_text())
+        for td in j.find_all('td')[1:]:
             if td.get('align'):
-                row_curr.append(td.get_text())
-    if len(row_curr) == len(col_name):
-        country_table.loc[edu_row_num] = row_curr
-        edu_row_num += 1
+                row_data.append(td.get_text())
+    if len(row_data) == len(col_name):
+        country_table.loc[edu_counter] = row_data
+        edu_counter += 1
 
 
-country_table[['Total', 'Men', 'Women']] = country_table[['Total', 'Men', 'Women']].convert_objects(
-    convert_numeric=True)
-print country_table
+
+sublistdf =country_table[['Total', 'Men', 'Women']]
+numeric_country_table= pd.to_numeric(sublistdf , errors='coerce')
+country_table[['Total', 'Men', 'Women']]= numeric_country_table[['Total', 'Men', 'Women']]
+
+#print numeric_country_table
+
 
 country_gdp = pd.DataFrame(
     columns=['country_name', '1999', '2000', '2001', '2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009',
@@ -84,38 +94,60 @@ with con:
     cur.execute(
         'CREATE TABLE gdp (country_name text,  _1999 numeric, _2000 numeric, _2001 numeric, _2002 numeric, _2003 numeric, _2004 numeric, _2005 numeric, _2006 numeric, _2007 numeric, _2008 numeric, _2009 numeric, _2010 numeric)')
 
-with open('/Users/jenniferconnolly/Documents/ThinkFul/ny/ny.gdp.mktp.cd_Indicator_en_csv_v2.csv', 'rU') as inputFile:
+with open('E:\\ThinkFul\\ny\\ny.gdp.mktp.cd_Indicator_en_csv_v2.csv', 'rU') as inputFile:
     next(inputFile)
     next(inputFile)
     header = next(inputFile)
-    print header
+    #print header
     inputReader = csv.reader(inputFile)
     gdp_row_num = 0
     for line in inputReader:
-        row_curr = [line[0]]
-        row_curr.extend(line[43:-5])
-        country_gdp.loc[gdp_row_num] = row_curr
+        row_data = [line[0]]
+        row_data.extend(line[43:-5])
+        country_gdp.loc[gdp_row_num] = row_data
         gdp_row_num += 1
         with con:
             cur.execute(
                 'INSERT INTO gdp (country_name, _1999, _2000, _2001, _2002, _2003, _2004, _2005, _2006, _2007, _2008, _2009, _2010) VALUES ("' +
                 line[0] + '","' + '","'.join(line[43:-5]) + '");')
 
-country_gdp[country_gdp.columns[1:-1]] = country_gdp[country_gdp.columns[1:-1]].convert_objects(convert_numeric = True)
 
-country_table['GDP'] = np.nan
-for i in range(edu_row_num):
-    find_index = country_gdp[country_gdp['country_name'] == country_table['Country or area'][i]].index
-    if len(find_index) > 0:
-        row_index = find_index.tolist()[0]
-        country_table['GDP'][i] = country_gdp[country_table['Year'][i]][row_index]
-edu_gdp = country_table[np.isfinite(country_table['GDP'])][['Country or area', 'Total', 'GDP']]
-edu_gdp['log_GDP'] = np.log(edu_gdp['GDP'])
 
-edu_gdp.plot(kind='scatter', x='Total', y='log_GDP')
-plt.show()
+sublist_gdp_df = country_gdp[country_gdp.columns[1:-1]]
+numeric_country_table= pd.to_numeric(sublist_gdp_df, errors='coerce')
+country_gdp[country_gdp.columns[1:-1]]=numeric_country_table
 
-gdp = country_table['GDP'].map(lambda x: float(x))
+country_table['GDP']=0
+
+
+#print country_gdp['GDP']
+#print country_gdp
+#print country_table
+
+gdplist =[]
+for i in range(edu_counter):
+    rowid = country_gdp[country_gdp['country_name'] == country_table['Country or area'][i]].index
+    if len(rowid) > 0:
+        row_index = rowid.tolist()[0]
+        try:
+            gdp = float(country_gdp[country_table['Year'][i]][row_index]) ##extracts year from country table they pull the year, row index from gdp table and assigns that value back to GDP col of the country tab;e
+            gdplist.append(gdp)
+        except ValueError:
+            gdp ='nan'
+            gdplist.append(gdp)
+
+    else:
+        gdp ='nan'
+        gdplist.append(gdp)
+
+#print gdplist
+#print len(gdplist)
+#print len(country_table['GDP'])
+
+country_table['GDP']= gdplist
+
+
+gdp = country_table['GDP'].map(lambda x: 0 if x =='nan'else float(x))
 school_years = country_table['Year'].map(lambda x: int(x))
 log_gdp = gdp.map(lambda x: np.log(x))
 
@@ -126,6 +158,7 @@ print len(school_years)
 colors = np.random.rand(len(gdp))
 plt.scatter(log_gdp, school_years, c=colors)
 plt.show()
+
 
 y = np.matrix(gdp).transpose()
 x = np.matrix(school_years).transpose()
@@ -146,12 +179,4 @@ plt.show()
 
 sm.graphics.tsa.plot_pacf(log_gdp) #Partial Autocorrelation
 plt.show()
-
-
-#So now you have the GDP for the countries and the school life expectancy, try and match them up.
-# See if there is any correlation between the GDP numbers and the life expectancy.
-# You may need to do a log-transform of the GDP to get a scale you can use to compare the widely distributed countries.
-# If there's a correlation, why do you think that is? Also explain why there may not be a correlation. Write up a blogpost
-# and share your insights with the world!
-
 
